b1de84bdbe87700103cb7e09e1bff71bd966de54
Sirion Feingold <sirionfeingold@users.noreply.github.com>
2025-07-06 22:09:46 +0200

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/msg_feingold_webseite/",
+  base: "/",
   plugins: [
     vue(), 
     tailwindcss()
