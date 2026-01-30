fb4c95ef8a1e8d82217bf27cf9d79404b763709b
Sirion Feingold <sirionfeingold@users.noreply.github.com>
2026-01-30 17:08:16 +0100

    update header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,8 +6,12 @@
 
         <!-- Logo und Titel -->
         <div class="header-brand">
-          <h1 class="header-logo">MSG Feingold</h1>
-          <h2 class="header-subtitle">Musik Seminar Gesellschaft Bern</h2>
+          <h1 class="header-logo">
+            {{ page?.acf?.header_logo }}
+          </h1>
+          <h2 class="header-subtitle">
+            {{ page?.acf?.header_subtitle }}
+          </h2>
         </div>
 
         <!-- Burger-Button für Sidebar -->
@@ -16,21 +20,49 @@
         <!-- Desktop-Navigation (optional anzeigen) -->
         <nav class="header-nav">
 
-          <router-link to="/" class="header-nav-link">Home</router-link>
-          <router-link to="/unterricht" class="header-nav-link">Unterricht</router-link>
+          <router-link to="/" class="header-nav-link">
+            {{ page?.acf?.header_home_text }}
+          </router-link>
+          <router-link to="/unterricht" class="header-nav-link">
+            {{ page?.acf?.header_unterricht_text }}
+          </router-link>
           <ShopDropDown />
-          <router-link to="/medien" class="header-nav-link">Medien</router-link>
-          <router-link to="/kontakt" class="header-nav-link">Kontakt</router-link>
-          <router-link to="/konditionen" class="header-nav-link">Konditionen</router-link>
-          <router-link to="/about" class="header-nav-link">Über uns</router-link>
+          <router-link to="/medien" class="header-nav-link">
+            {{ page?.acf?.header_medien_text }}
+          </router-link>
+          <router-link to="/kontakt" class="header-nav-link">
+            {{ page?.acf?.header_kontakt_text }}
+          </router-link>
+          <router-link to="/konditionen" class="header-nav-link">
+            {{ page?.acf?.header_konditionen_text }}
+          </router-link>
+          <router-link to="/about" class="header-nav-link">
+            {{ page?.acf?.header_about_text }}
+          </router-link>
         </nav>
       </div>
     </header>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import { loadPage } from '../api/wp';
 import ShopDropDown from './ShopDropDown.vue';
 import Sidebar from './Sidebar.vue';
+
 const sidebarOpen = ref(false)
+const page = ref<any>(null)
+const loading = ref(true)
+const error = ref<string | null>(null)
+
+onMounted(async () => {
+    try {
+      page.value = await loadPage('header') // slug aus WP
+    } catch (e: any) {
+      error.value = e?.message ?? 'Fehler beim Laden'
+    } finally {
+      loading.value = false
+    }
+  })
+
 </script>
