13273a3a31eed5090fe0c11c56871313daf4375a
Sirion Feingold <sirionfeingold@users.noreply.github.com>
2026-06-14 12:59:55 +0200

    update dist

--- Todo.md
+++
@@ -1,6 +0,0 @@
-** Todos **
-1. Online shop --> done
-2. Sidebar --> done
-3. Instrument.vue --> text
-4. Bewertungen --> done
-5. Mail functionality
--- src/shims-vue.d.ts
+++
@@ -1,7 +0,0 @@
-// File to tell ts that vue files are valid moduls
-
-declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
